e434b4b28e1bbac649e09b1f5977d6a1157e3770
Satiwan Kumar <2476@salsoft.local>
2021-01-13 19:45:45 +0500

    updated

--- packages/shop/pages/_app.tsx
+++ packages/shop/pages/_app.tsx
@@ -54,7 +54,7 @@ export default function ExtendedApp({
   return (
     <ThemeProvider theme={theme}>
         <Head>
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" />
                 <link 
                     rel="stylesheet" 
                     href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
