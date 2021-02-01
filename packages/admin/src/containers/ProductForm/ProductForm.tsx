bf59c7eb01a9f053d3464d7d9e6d482a4cd1c78e
Charles Smith <charlestsmith888gmail.com>
2021-02-01 18:00:01 +0500

    this commit

--- packages/admin/src/containers/ProductForm/ProductForm.tsx
+++ packages/admin/src/containers/ProductForm/ProductForm.tsx
@@ -388,12 +388,31 @@ data && data.getproducts.map(item=>
                     onChange={((e)=>handleVariationChange(e,i))}
                     name="variation_sell_price" className="form-control brand-flied"/>
             </Col>
-                <Col xl={3} lg={6} md={6}>
+                <Col md={12}>
                 {
-   i == (variation.length - 1)?  (<><Button type="button"   startEnhancer={() => <Plus />} title="add variation"  onClick={()=>AddVariation()} className="nm-bt mt-40"></Button>
-   <Button type="button" title="add variation"  startEnhancer={() => <Trash />} onClick={()=>removeVariation(i)} className="nm-bt mt-40 red-bg"></Button></>
+   i == (variation.length - 1)?  (
+   
+   <>
+  <Row>
+  <Col xl={3} lg={6} md={6}>
+   <Button type="button"   startEnhancer={() => <Plus />} title="add variation"  onClick={()=>AddVariation()} className="nm-bt mt-40"></Button>
+   </Col>
+   <Col xl={3} lg={6} md={6}>
+   <Button type="button" title="add variation"  startEnhancer={() => <Trash />} onClick={()=>removeVariation(i)} className="nm-bt mt-40 red-bg"></Button>
+   </Col>
+   
+   </Row>
+   </>
    )
-   :(<Button type="button" title="add variation"  startEnhancer={() => <Trash />} onClick={()=>removeVariation(i)} className="nm-bt mt-40 red-bg"></Button>)
+   :(
+   <Row>
+  <Col xl={3} lg={6} md={6}>
+
+   <Button type="button" title="add variation"  startEnhancer={() => <Trash />} onClick={()=>removeVariation(i)} className="nm-bt mt-40 red-bg"></Button>
+  </Col>
+
+   </Row>
+   )
 
                 }
      
